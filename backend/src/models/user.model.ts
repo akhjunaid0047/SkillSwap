6a75ac55a848186a0d83e166950e24427e87784a
akhjunaid0047 <akhjunaid.047+github@gmail.com>
2025-07-12 12:13:15 +0530

    user model changes

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,29 +1,38 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, {Schema, Document} from 'mongoose';
 
 export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
   location?: string;
-  profile?: string;
+  profile?: 'private' | 'public';
   profilePhoto?: string;
-  availability: string[];
+  availability: 'weekdays' | 'weekends';
   skillsOffered: string[];
   skillsWanted: string[];
   rating: number;
 }
 
 const UserSchema: Schema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  location: { type: String },
-  profile: { type: String },
-  profilePhoto: { type: String },
-  availability: [{ type: String }],
-  skillsOffered: [{ type: String }],
-  skillsWanted: [{ type: String }],
-  rating: { type: Number, default: 0, min: 0, max: 5 },
+  name: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  location: {type: String},
+  profile: {
+    type: String,
+    enum: ['private', 'public'],
+    default: 'public',
+  },
+  profilePhoto: {type: String},
+  availability: [
+    {
+      type: String,
+      enum: ['weekdays', 'weekends'],
+    },
+  ],
+  skillsOffered: [{type: String}],
+  skillsWanted: [{type: String}],
+  rating: {type: Number, default: 0, min: 0, max: 5},
 });
 
 export default mongoose.model<IUser>('User', UserSchema);
